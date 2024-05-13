d45bc805d2ebf12d4f4f307dd826ae24407d3ab5
MatiPachame <matipachame16@gmail.com>
2024-05-13 14:55:30 -0300

    Agregando loading

--- src/app/componentes/principal/principal.component.ts
+++ src/app/componentes/principal/principal.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "../menu/menu.component";
 import { RouterModule } from '@angular/router';
+import { LoadingComponent } from "../loading/loading.component";
 
 @Component({
     selector: 'app-principal',
     standalone: true,
-    imports: [RouterModule, MenuComponent],
     templateUrl: './principal.component.html',
     styleUrl: './principal.component.css',
-    
+    imports: [RouterModule, MenuComponent, LoadingComponent]
 })
 export class PrincipalComponent {
 
