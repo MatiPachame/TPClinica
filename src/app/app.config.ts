0b38d58d28e80391fcb4684c14387df3ba9aa667
MatiPachame <matipachame16@gmail.com>
2024-08-12 09:54:03 -0300

    agregando token

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes),
+    importProvidersFrom(
+      JwtModule.forRoot({
+          config: {
+              tokenGetter: tokenGetter,
+              allowedDomains: ["example.com"],
+              disallowedRoutes: ["http://example.com/examplebadroute/"],
+          },
+      }),
+  ), provideHttpClient(withInterceptorsFromDi())]
 };
+
+export function tokenGetter() {
+  return localStorage.getItem("UserToken");
+}
--- src/app/componentes/nuevo-turno/nuevo-turno.component.ts
+++ src/app/componentes/nuevo-turno/nuevo-turno.component.ts
@@ -67,17 +67,6 @@ export class NuevoTurnoComponent {
 }
 
 
-  // public formatDateForMySQL(date: Date): string {
-  //   const year = date.getFullYear();
-  //   const month = ('0' + (date.getMonth() + 1)).slice(-2); // Añadir cero a la izquierda si es necesario
-  //   const day = ('0' + date.getDate()).slice(-2);
-  //   const hours = ('0' + date.getHours()).slice(-2);
-  //   const minutes = ('0' + date.getMinutes()).slice(-2);
-  //   const seconds = ('0' + date.getSeconds()).slice(-2);
-  
-  //   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-  // }
-
   public buscarDisponibilidad(){
     const diasSemana = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes'];
     const hoy = new Date();
