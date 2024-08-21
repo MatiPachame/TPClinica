8821abf14167cfbb8d76d9f59f44a1b673684dda
MatiPachame <matipachame16@gmail.com>
2024-08-21 15:02:59 -0300

    Agregando el descargar a pdf, faltan arreglos

--- src/app/servicios/passtopdf.service.ts
+++ src/app/servicios/passtopdf.service.ts
@@ -12,12 +12,35 @@ export class PasstopdfService {
 
   exportAsPDF(divId: string) {
     let data = document.getElementById(divId);
-    html2canvas(data!).then(canvas => {
+
+    // Mostrar el contenedor antes de capturarlo
+    if (data) {
+        data.style.display = 'block';
+    }
+
+    html2canvas(data!, { scale: 2 }).then(canvas => {
       const contentDataURL = canvas.toDataURL('image/png');
       let pdf = new jspdf('p', 'cm', 'a4');
-      pdf.addImage(contentDataURL, 'PNG', 0, 0, 21.0, 29.7);
+
+      // Obtener las dimensiones del canvas
+      let imgWidth = 21.0; // Ancho de la imagen (en cm)
+      let imgHeight = canvas.height * imgWidth / canvas.width; // Calcular la altura manteniendo la proporción
+
+      // Si la altura calculada es mayor que la altura de la página, ajusta la altura
+      if (imgHeight > 29.7) {
+          imgHeight = 29.7;
+          imgWidth = canvas.width * imgHeight / canvas.height;
+      }
+
+      pdf.addImage(contentDataURL, 'PNG', 0, 0, imgWidth, imgHeight);
       pdf.save('ListaMedicos.pdf');
+
+        // Volver a ocultar el contenedor después de capturarlo
+        if (data) {
+            data.style.display = 'none';
+        }
     });
-  }
+}
+
 
 }
